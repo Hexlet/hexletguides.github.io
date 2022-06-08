f9c3d247b48c9cd7f32aadef8d0030497e9666c3
elisad5791 <elisad5791@mail.ru>
2022-06-08 13:49:22 +0500

    [#365] add styles

---
+++ next-app/pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/theme.scss';
+import '../styles/highlight.css';
+import '../styles/custom.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
