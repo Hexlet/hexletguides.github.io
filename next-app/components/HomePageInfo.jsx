fe5a264b9813dd2fec7a05ae33c55088bdfe5040
Roman Gafurov <gafurov.roma89@gmail.com>
2022-07-13 17:07:44 +0300

    Remove alt for decorative image

--- next-app/components/HomePageInfo.jsx
+++ next-app/components/HomePageInfo.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import Image from 'next/image';
 import { useTranslation } from 'next-i18next';
 
-
 const HomePageInfo = () => {
   const { t } = useTranslation();
+
   return (
-      <div className="rounded hero p-4 p-lg-5 mb-5">
-        <div className="row mx-lg-5">
-          <div className="col-md-8">
-            <h1 className="display-5 fw-bold">{t('hero.header')}</h1>
-            <p className="lead">{t('hero.description')}</p>
-          </div>
-          <div className="col-md-4 d-none d-md-block">
-            <Image className="intro" width="344" height="274" src="/images/intro.svg" alt="Intro image" />
-          </div>
+    <div className="rounded hero p-4 p-lg-5 mb-5">
+      <div className="row mx-lg-5">
+        <div className="col-md-8">
+          <h1 className="display-5 fw-bold">{t('hero.header')}</h1>
+          <p className="lead">{t('hero.description')}</p>
+        </div>
+        <div className="col-md-4 d-none d-md-block">
+          <Image className="intro" width="344" height="274" src="/images/intro.svg" alt="" />
         </div>
       </div>
+    </div>
   );
 };
 
