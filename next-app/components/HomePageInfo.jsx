e40737effb9bd8e69c67c2145b94e424170a65b3
Roman Gafurov <gafurov.roma89@gmail.com>
2022-07-22 18:15:33 +0300

    Added posts list

--- next-app/components/HomePageInfo.jsx
+++ next-app/components/HomePageInfo.jsx
@@ -2,21 +2,34 @@ import React from 'react';
 import Image from 'next/image';
 import { useTranslation } from 'next-i18next';
 
-const HomePageInfo = () => {
+import Post from './Post.jsx';
+
+const HomePageInfo = ({ posts }) => {
   const { t } = useTranslation();
 
+  const postsList = posts.map(({ header, summary, name }) => (
+    <div key={name} className="col-md-6 mb-5">
+      <Post name={name} header={header} summary={summary} />
+    </div>
+  ));
+
   return (
-    <div className="rounded hero p-4 p-lg-5 mb-5">
-      <div className="row mx-lg-5">
-        <div className="col-md-8">
-          <h1 className="display-5 fw-bold">{t('hero.header')}</h1>
-          <p className="lead">{t('hero.description')}</p>
-        </div>
-        <div className="col-md-4 d-none d-md-block">
-          <Image className="intro" width="344" height="274" src="/images/intro.svg" alt="" />
+    <>
+      <div className="rounded hero p-4 p-lg-5 mb-5">
+        <div className="row mx-lg-5">
+          <div className="col-md-8">
+            <h1 className="display-5 fw-bold">{t('hero.header')}</h1>
+            <p className="lead">{t('hero.description')}</p>
+          </div>
+          <div className="col-md-4 d-none d-md-block">
+            <Image className="intro" width="344" height="274" src="/images/intro.svg" alt="" />
+          </div>
         </div>
       </div>
-    </div>
+      <div className="row mx-lg-5">
+        {postsList}
+      </div>
+    </>
   );
 };
 
