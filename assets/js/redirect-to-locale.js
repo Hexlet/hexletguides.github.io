36dd000daefd25359e00595438fbe4b5c8fa5b87
TimurDavlet <timkoootim@yandex.ru>
2022-09-16 11:52:52 +0400

    update redirect file

--- assets/js/redirect-to-locale.js
+++ assets/js/redirect-to-locale.js
@@ -1,27 +1,21 @@
 function getBestSuitableSupportedLang(lang, supported) {
-  var supported_lang = supported.shift();
-
+  let supported_lang = supported.shift();
+ 
   if (supported.includes(lang)) {
     supported_lang = lang;
-  } else if (supported.includes(lang)) {
-    supported_lang = lang;
   }
-
+ 
   return supported_lang;
 }
-
-var [lang] = (((navigator.userLanguage || navigator.language).replace('-', '_')).toLowerCase()).split('_');
-
-var supported_languages = ['en', 'ru'];
-var current_lang = 'en';
-
-var suitable_lang = getBestSuitableSupportedLang(lang, supported_languages)
-
-var hostname = window.location.hostname;
-var referrer = document.referrer;
-
-var landingPage = !referrer || referrer.indexOf(hostname) == -1;
-
-if (landingPage && (current_lang !== suitable_lang)) {
+ 
+const [lang] = (((navigator.userLanguage || navigator.language).replace('-', '_')).toLowerCase()).split('_');
+const supported_languages = ['en', 'ru'];
+const current_lang = 'en';
+const suitable_lang = getBestSuitableSupportedLang(lang, supported_languages)
+const hostname = window.location.hostname;
+const referrer = document.referrer;
+const landingPage = !referrer || referrer.indexOf(hostname) == -1;
+ 
+  if (landingPage && (current_lang !== suitable_lang)) {
   window.location = '/' + suitable_lang + '/';
 }
