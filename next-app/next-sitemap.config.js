f081d5771374d6600184a2f12435805b570001ca
fey <fey@users.noreply.github.com>
2022-09-23 12:03:38 +0000

    deploy: 72ec73638c8c5058ad2ff46ab4cfb667a2664677

--- next-app/next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  siteUrl: 'https://guides.hexlet.io',
-  generateIndexSitemap: false,
-  changefreq: null,
-  priority: null,
-};
--- ru/next-app/components/PostPageNav.jsx
+++ ru/next-app/components/PostPageNav.jsx
@@ -2,30 +2,20 @@ import React from 'react';
 import Link from 'next/link';
 import { useTranslation } from 'next-i18next';
 
-const makeHref = (name) => ({
-  pathname: '/[name]',
-  query: {
-    name,
-  },
-});
-
 const PostPageNav = ({ nextPost, prevPost }) => {
   const { t } = useTranslation('post');
-
-  const prevHref = makeHref(prevPost.name);
-  const nextHref = makeHref(nextPost.name);
 
   return (
     <div className="row PageNavigation mt-4 prevnextlinks d-flex justify-content-between">
       <div className="col-md-6 rightborder pl-0">
-        <Link href={prevHref}>
+        <Link href={prevPost.href}>
           <a alt={t('page.prev_guide')} className="thepostlink">
             « {prevPost.header}
           </a>
         </Link>
       </div>
       <div className="col-md-6 text-end pr-0">
-        <Link href={nextHref}>
+        <Link href={nextPost.href}>
           <a alt={t('page.next_guide')} className="thepostlink">
             {nextPost.header} »
           </a>
