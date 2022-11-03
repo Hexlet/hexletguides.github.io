bdecdfcea2d2be0c1376a21c840abb0642b385d4
Dmitriy M <d.mir.posts@gmail.com>
2022-11-03 12:03:18 +0300

    [#529] init sentry

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 const { i18n } = require('./next-i18next.config.js');
+const { withSentryConfig } = require('@sentry/nextjs');
 
-module.exports = {
-  i18n,
-  trailingSlash: true,
-  experimental: {
-    amp: {
-      skipValidation: true,
+const sentryWebpackPluginOptions = {
+  silent: true,
+};
+
+module.exports = withSentryConfig(
+  {
+    i18n,
+    trailingSlash: true,
+    experimental: {
+      amp: {
+        skipValidation: true,
+      },
+    },
+    sentry: {
+      hideSourceMaps: true,
     },
   },
-};
+  sentryWebpackPluginOptions,
+);
