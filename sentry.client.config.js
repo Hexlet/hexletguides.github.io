61cfc3c63169c1c78e49eb71edde3806c4785c4c
Dmitriy M <d.mir.posts@gmail.com>
2022-11-07 22:11:13 +0300

    [529] delete private tokens

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://90e93892c3d84cba9aa972d1ea35bb7c@o4504088994054144.ingest.sentry.io/4504089089081344',
+  dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
