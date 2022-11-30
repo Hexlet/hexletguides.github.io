739129153542f87adc12d114758e651aa1e595e2
HunterGan <101088097+HunterGan@users.noreply.github.com>
2022-11-30 10:08:46 +0300

    Update MicrometricArticles.jsx set indents to code
    Set 2 spaces indents

--- components/MicrometricArticles.jsx
+++ components/MicrometricArticles.jsx
@@ -3,33 +3,33 @@ import React from 'react';
 import cfg from '../data/config.js';
 
 const makeConfig = ({ author, header = '', date = '', image = '', author_url = '' }) => {
-    if (!author) return null;
-    const articleImage = image ? `${cfg.siteUrl}${image}` : '';
-    const result = {
-        "@context": "https://schema.org",
-        "@type": "Article",
-        "headline": header,
-        "image": articleImage,
-        "datePublished": date,
-        "author": {
-            "@type": "Person",
-            "name": author,
-            "url": author_url,
-        },
-    }
-    return result;
+  if (!author) return null;
+  const articleImage = image ? `${cfg.siteUrl}${image}` : '';
+  const result = {
+    "@context": "https://schema.org",
+    "@type": "Article",
+    "headline": header,
+    "image": articleImage,
+    "datePublished": date,
+    "author": {
+      "@type": "Person",
+      "name": author,
+      "url": author_url,
+    },
+  }
+  return result;
 };
 
 const MicrometricArticles = ({ post }) => {
-    const config = makeConfig(post);
-    return config && (
-        <script
-            type="application/ld+json"
-            dangerouslySetInnerHTML={{
-                __html: JSON.stringify(makeConfig(post)),
-            }}
-        />
-    );
+  const config = makeConfig(post);
+  return config && (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify(makeConfig(post)),
+        }}
+    />
+  );
 }
 
 export default MicrometricArticles;
